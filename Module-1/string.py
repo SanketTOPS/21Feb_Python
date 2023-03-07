03bf70c88156d19c4dc974fd143e65089d95e4c2
SanketTops <sanket.tops@gmail.com>
2023-03-07 14:49:37 +0530

    String

---
+++ Module-1/string.py
@@ -0,0 +1,37 @@
+mystr="this is python"
+
+"""print(mystr)
+print(mystr[0])
+print(mystr[-1])
+print(mystr[0:4]) #string slice - range
+print(mystr[8:])
+print(mystr[:4])
+print(len(mystr))"""
+
+# ----------------------------------------------- #
+# String Methods
+
+print(mystr)
+#print(mystr.strip())
+#print(mystr.lower())
+#print(mystr.upper())
+#print(mystr.replace('This','That'))
+#print(mystr.replace('i','I'))
+
+
+#print(mystr.capitalize())
+#print(mystr.casefold())
+#print(mystr.count('is'))
+#print(mystr.startswith('T'))
+#print(mystr.endswith('!'))
+#print(mystr.index('Python'))
+#print(mystr.find('P'))
+#print(mystr.isalpha())
+#print(mystr.isdigit())
+#print(mystr.isalnum())
+#print(mystr.islower())
+#print(mystr.isupper())
+#print(mystr.isnumeric())
+#print(mystr.isdecimal())
+#print("\U0001f608")
+print(mystr.title())
